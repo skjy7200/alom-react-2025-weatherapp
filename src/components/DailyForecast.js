1e950f5b4e13824f24bfa7e5315421057c4f79c9
김강현 <skjy7200@gmail.com>
2025-05-19 08:21:37 +0900

    Update DailyForecast.js

--- src/components/DailyForecast.js
+++ src/components/DailyForecast.js
@@ -1,11 +1,24 @@
 import React from "react";
-import { DailyForecastWrapper, DailyItem } from "./styles/StyledComponents";
+import {
+  DailyForecastWrapper,
+  DailyItem,
+} from "./styles/StyledComponents";
 import { getWeatherDescription, formatDailyData } from "../utils/weather";
 
 const DailyForecast = ({ weatherData }) => {
   const dailyData = formatDailyData(weatherData);
 
-  return <div>채워주세요</div>;
+  return (
+    <DailyForecastWrapper>
+      {dailyData.map((item, idx) => (
+        <DailyItem key={idx}>
+          <div>{item.date}</div>
+          <div>{getWeatherDescription(item.code)}</div>
+          <div>{item.temp}°C</div>
+        </DailyItem>
+      ))}
+    </DailyForecastWrapper>
+  );
 };
 
 export default DailyForecast;
